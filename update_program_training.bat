c0f6672c248fbeff3c776a6526ccb13c3868ae05
Duc Anh TA <83185055+taducanh1605@users.noreply.github.com>
2022-08-03 17:12:10 +0200

    Update update_program_training.bat

--- update_program_training.bat
+++ update_program_training.bat
@@ -2,3 +2,4 @@
 python "./Training Program/csvTojson.py" "Training Program\\FullBodyMale"
 python "./Training Program/csvTojson.py" "Training Program\\FullBodyFemale"
 python "./Training Program/csvTojson.py" "Training Program\\FullBodyPers"
+python "./Training Program/csvTojson.py" "Training Program\\Calisthenic"
